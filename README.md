4e9e49a9b3b69a31dce11dcaa0afde5d04a8a475
shredinjohn <josephjohnphilip@gmail.com>
2024-04-25 12:20:01 +0530

    main

---
+++ README.md
@@ -0,0 +1,1 @@
+This is FAT LAB TEST
---
+++ test.java
@@ -0,0 +1,18 @@
+public class test {
+    public static void main(String[] args) {
+         
+        int firstNumber = 10;
+        // Prompt the user to enter the first number
+        System.out.print("First number: " + firstNumber +"\n");
+       
+        int secondNumber = 20;
+        // Prompt the user to enter the second number
+        System.out.print("Second number: "+ secondNumber+"\n");
+
+        // Calculate the sum of the two numbers
+        double sum = firstNumber + secondNumber;
+
+        // Print the sum of the two numbers
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+    }
+}
